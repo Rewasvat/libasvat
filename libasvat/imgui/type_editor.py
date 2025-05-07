00fce8a7abab1aca6376713d86fd511ef9367d3f
Fernando Omar Aluani <fernando.aluani@tapps.com.br>
2025-05-07 15:49:02 -0300

    Implemented EditorController class to allow easy editing of a object in imgui using its TypeEditors

--- libasvat/imgui/type_editor.py
+++ libasvat/imgui/type_editor.py
@@ -1,4 +1,6 @@
+import copy
 import types
+import click
 import typing
 import inspect
 import libasvat.command_utils as cmd_utils
@@ -1396,3 +1398,127 @@ def restore_prop_values_to_object(obj, values: dict[str]):
         props[key].restore_value(obj, value)
     return issues
 
+
+class EditorController:
+    """Direct object/value editor in IMGUI.
+
+    This is a utility class that allows to edit a specific object in IMGUI, associating the object with its TypeEditor.
+
+    TypeEditors are usually directed into editing a value as a property of another object. This utility class allows to
+    easily use it to directly edit a value. So this EditorController is usually used as the starting point in a GUI to edit
+    a object.
+
+    As such, this class allows one to indicate the user will start to edit the object, and then accept or cancel the edit.
+    When cancelling, the object's property values are restored to their original values (when ``start_edit()`` was called).
+
+    The object being edited can implement any of the following optional methods, all of which receive the EditorController's
+    instance as argument, in order to expand the editor's features:
+    * ``_on_render_editor(EditorController)``: called each frame when rendering this EditorController (see ``render_editor()``).
+    Can be used to add custom-control to the rendered editor.
+    * ``_on_start_edit(EditorController)``: called when editing is started (see ``start_edit()``).
+    * ``_on_accept_edit(EditorController)``: called when the user accepts the edits he's made (see ``accept_edit()``).
+    * ``_on_cancel_edit(EditorController)``: called when the user cancels the edits he's made (see ``cancel_edit()``).
+    """
+
+    def __init__(self, obj, editor_config: dict[str, any] = None, obj_type: type = None):
+        """
+        Args:
+            obj (any): the object to edit.
+            editor_config (dict[str,any], optional): the optional config dict for the object's TypeEditor.
+            obj_type (type, optional): specific object type to use for getting a TypeEditor from the TypeDatabase. If None,
+                will use ``type(obj)``.
+        """
+        self.obj = obj
+        obj_type = obj_type or type(obj)
+        self.editor = TypeDatabase().get_editor(obj_type, editor_config)
+        self._is_editing: bool = False
+        self._backup_prop_values: dict[str, any] = {}
+        self.show_tips: bool = True
+        """If a few common imgui tips should be shown in the "Editor Commands" section of the editor."""
+        self.use_new_line: bool = True
+        """If empty lines should be added before and after the object's editor controls, in order to improve readability."""
+
+    @property
+    def is_editing(self) -> bool:
+        """Indicates if this EditorControl is currently editing the object."""
+        return self._is_editing
+
+    def render_editor(self):
+        """Renders the editor for the object. Does nothing if not editing.
+
+        This should be called every frame, to allow the editor to render its contents, and allow the user
+        to edit the object.
+
+        This rendering has the following steps or sections:
+        * **Title**: Separator-text with the object's string value
+        * **Content**: renders the object's TypeEditor.
+        * **Tips**: renders some common editor tips.
+        * **Footer**: contains the CANCEL/ACCEPT buttons, to cancel/accept the edit.
+        """
+        if not self._is_editing:
+            return
+        imgui.separator_text(f"{self.obj} Editor")
+        if self.use_new_line:
+            imgui.new_line()
+        changed, new_value = self.editor.render_value_editor(self.obj)
+        if changed:
+            self.obj = new_value
+        if hasattr(self.obj, "_on_render_editor"):
+            self.obj._on_render_editor(self)
+
+        if self.use_new_line:
+            imgui.new_line()
+        imgui.separator_text("Editor Commands")
+        if self.show_tips:
+            imgui.text_wrapped(f"Tip #1: hovering the mouse over a property-name or value editor control usually shows a tooltip with more information.")
+            imgui.text_wrapped(f"Tip #2: most number controls can be edited by click & dragging the mouse over them.")
+        with imgui_ctx.begin_horizontal(f"{self.obj}EditorControllerButtons"):
+            if adv_button("Cancel", tooltip="Closes the editor, cancelling the current edit and restoring the original values."):
+                self.cancel_edit()
+            if adv_button("Accept", tooltip="Closes the editor, accepting the current edit and keeping the new values."):
+                self.accept_edit()
+
+    def start_edit(self):
+        """Starts editing the object, allowing the user to change its values.
+        Current property values of the object are saved so they can be restored later, if edit is cancelled.
+
+        This method also calls the ``_on_start_edit(EditorController)`` method of the object, if it exists.
+        """
+        if self._is_editing:
+            return
+        self._is_editing = True
+        self._backup_prop_values.clear()
+        self._backup_prop_values = copy.deepcopy(get_all_prop_values_for_storage(self.obj))
+        if hasattr(self.obj, "_on_start_edit"):
+            self.obj._on_start_edit(self)
+
+    def accept_edit(self):
+        """Accepts the current edit, allowing the edited values to remain set in the object.
+        Does nothing if not editing.
+
+        This method also calls the ``_on_accept_edit(EditorController)`` method of the object, if it exists.
+        """
+        if not self._is_editing:
+            return
+        self._is_editing = False
+        # Edited values should already be set in the object, so all we need to do is clear the backup values.
+        self._backup_prop_values.clear()
+        if hasattr(self.obj, "_on_accept_edit"):
+            self.obj._on_accept_edit(self)
+
+    def cancel_edit(self):
+        """Cancels the current edit, restoring the original values of the object.
+        Does nothing if not editing.
+
+        If any issues are found when restoring the values, they are printed to the console.
+
+        This method also calls the ``_on_cancel_edit(EditorController)`` method of the object, if it exists.
+        """
+        if not self._is_editing:
+            return
+        self._is_editing = False
+        issues = restore_prop_values_to_object(self.obj, self._backup_prop_values)
+        for msg in issues:
+            click.secho(msg, fg="yellow")
+        if hasattr(self.obj, "_on_cancel_edit"):
+            self.obj._on_cancel_edit(self)
