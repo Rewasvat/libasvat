558ffcc186007ad8fcf05b2c9b6b3f3b123da0f2
Fernando Omar Aluani <fernando.aluani@tapps.com.br>
2025-07-17 16:29:25 -0300

    Updated google-sheet module with changes added in Maestro: - updated way of getting API service object with credentials - added new API methods

--- libasvat/google_sheet.py
+++ libasvat/google_sheet.py
@@ -5,6 +5,7 @@ import socket
 import traceback
 from typing import Iterator
 from libasvat.data import DataCache
+from libasvat.utils import Table
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 from google.auth.transport.requests import Request
@@ -192,7 +193,19 @@ class Row:
 
 
 class Sheet:
-    """Creates a Sheet object with methods to facilitate working with Google Spreadsheets."""
+    """Creates a Sheet object with methods to facilitate working with Google Spreadsheets.
+
+    Notes on terminology:
+    * Google Sheets API uses the term `spreadsheet` to refer to the actual file that may contain one or more
+    tables, each table being a "tab" on the file, with its own title. These tables or tabs are then called `sheet` by the API.
+    So a `spreadsheet` is a collection of `sheet`s.
+    * In here, we use the term `sheet` more generically, both to mean the "file" (the `spreadsheet` in Google's term) and the
+    "table" (the `sheet` in Google's term). At the moment, documentation for attributes/methods/etc should specify exactly
+    what `sheet` means in that context (the file or the table). This ammbiguity should remain until we refactor this module.
+
+    This Sheet class represents a single table (`sheet`) in a spreadsheet (file). To identify the sheet, we use the (spread)sheet ID
+    and the table's name.
+    """
 
     _service_obj: discovery.Resource = None
     """The Google Sheets API service object. This object is initialized once by a Sheet, and reused for all other Sheets
@@ -201,15 +214,21 @@ class Sheet:
     _default_creds: 'SheetCredentials' = None
     """Default SheetCredentials object to use for authenticating Sheet API requests."""
 
-    def __init__(self, sheet_id: str, sheet_name: str, creds: 'SheetCredentials', verbose=False):
-        """* SHEET_ID: sheet hash code. Usually found in URL.
-        * SHEET_NAME: the individual table name from the sheet to load.
-        * CREDS: the SheetCredentials object to use for authenticating access to this Sheet. This can be None if the Sheet's API ServiceObject
-        is already loaded.
-        * VERBOSE: enable verbose logging.
+    def __init__(self, sheet_id: str, sheet_name: str, creds: 'SheetCredentials' = None, verbose=False):
+        """
+        Args:
+            sheet_id (str): sheet hash code. Usually found in URL.
+            sheet_name (str): the individual table name from the sheet to load.
+            creds (SheetCredentials, optional): the SheetCredentials object to use for authenticating access to this Sheet.
+                This can be None if the Sheet's API ServiceObject is already loaded. If None, we'll try to use the default
+                credentials (see ``Sheet.set_default_credentials()``).
+            verbose (bool, optional): enable verbose logging.
         """
         self.sheet_id = sheet_id
+        """The ID of the spreadsheet. This identifies the sheet "file" in Google Drive, and the file can have multiple sheets (tabs),
+        identified by their name (see ``sheet_name``) or more rarely, their ID."""
         self.sheet_name = sheet_name
+        self._table_id: int = None
         self.rows: list[Row] = []
         """The list of rows of this sheet. Each row itself is a list of Cells, ordered from first column to last."""
         self.header: Row = Row(self, -1, [])
@@ -249,7 +268,7 @@ class Sheet:
         self.rows.append(row)
         return row
 
-    def get_cell(self, key):
+    def get_cell(self, key: str):
         """Gets the cell by its KEY - its A1 notation index."""
         # TODO: allow key ranges as in sheets to return a list of cells?
         values = re.match(r"^([a-zA-Z]+)(\d+)$", key)
@@ -264,7 +283,7 @@ class Sheet:
         cell_index = letterToColumn(column_letter) - 1
         return row[cell_index]
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str):
         return self.get_cell(key)
 
     def __setitem__(self, key, value):
@@ -279,12 +298,19 @@ class Sheet:
         return len(self.get_rows())
 
     @property
+    def _service(self):
+        """Internal authenticated google Resource object to access the Sheets API."""
+        if self._credentials:
+            return self._credentials.get_service()
+        return self._default_creds.get_service()
+
+    @property
     def is_loaded(self):
         """Checks if this sheet is already loaded."""
         return self._is_loaded
 
     def load(self):
-        """Loads the sheet data from this object, using the user's login for authentication.
+        """Loads the sheet data from this object, using our credentials for authentication.
         Returns a boolean indicating if loading was successfull or not."""
         retry = 0
         max_retries = 3
@@ -295,8 +321,7 @@ class Sheet:
             try:
                 ranges = [f"'{self.sheet_name}'!A1:ZZ"]
 
-                service = self._get_service(self._credentials)
-                result = service.spreadsheets().values().batchGet(spreadsheetId=self.sheet_id, ranges=ranges).execute()
+                result = self._service.spreadsheets().values().batchGet(spreadsheetId=self.sheet_id, ranges=ranges).execute()
                 for index, row_data in enumerate(result["valueRanges"][0].get('values', [])):
                     self.rows.append(Row(self, index, row_data))
                 self.set_header_row(0)
@@ -335,8 +360,7 @@ class Sheet:
 
             num_changes = len(body["data"])
             if num_changes > 0:
-                service = self._get_service(self._credentials)
-                service.spreadsheets().values().batchUpdate(spreadsheetId=self.sheet_id, body=body).execute()
+                self._service.spreadsheets().values().batchUpdate(spreadsheetId=self.sheet_id, body=body).execute()
                 self._log(f"Sheet '{self.sheet_name}': saved changes in {num_changes} cells", fg="green")
             else:
                 self._log(f"Sheet '{self.sheet_name}': no changes to save", fg="green")
@@ -345,33 +369,122 @@ class Sheet:
             self._log(f"Couldn't write to sheet '{self.sheet_name}': {traceback.format_exc()}", fg="red", ignore_verbose=True)
             return False
 
-    @classmethod
-    def _get_service(cls, creds: 'SheetCredentials') -> discovery.Resource:
-        """Gets the Google's Resource service, used to call Sheets API commands.
+    def duplicate(self, target_sheet_id: str):
+        """Duplicates this sheet to the target spreadsheet.
 
-        This uses the given Google Credentials to authenticate the Sheets API commands.
+        The duplicated sheet will be a new sheet (table) at the target spreadsheet. It is a perfect copy: all cell
+        values, formatting, validation rules, etc, are copied. The sheet's name is the same name as this source
+        sheet, but with a `Copy of` prefix.
+
+        Note however that only sheet-level data is copied, since we're copying a sheet. Spreadsheet-level data, such
+        as user access permissions, are not copied. The auth credentials used to access this sheet also need access
+        to the target spreadsheet in order to duplicate this sheet there.
+
+        Implementation wise, this uses the `copyTo` command from the API, which can be directly used as `Copy to >`
+        in the browser's Google Sheets editor.
+
+        Args:
+            target_sheet_id (str): Target spreadsheet ID to copy this sheet to. This can be the same sheet-ID as
+                this sheet, in order to duplicate it in the same spreadsheet.
+
+        Returns:
+            Sheet: a new Sheet object representing the duplicated sheet, if successful, None otherwise (error
+            messages are automatically printed to the terminal).
         """
-        if creds is None:
-            creds = cls._default_creds
-        if cls._service_obj is None:
-            discovery_url = "https://sheets.googleapis.com/$discovery/rest?version=v4"
-            # For now use the same scopes for all sheets since its easier and there's been no need for custom scopes per-sheet.
-            scopes = [
-                'https://www.googleapis.com/auth/spreadsheets.readonly',
-                'https://www.googleapis.com/auth/spreadsheets'
-            ]
-            google_creds = creds.get_credentials(scopes)
-            cls._service_obj = discovery.build('sheets', 'v4', credentials=google_creds, discoveryServiceUrl=discovery_url, cache_discovery=False)
-        return cls._service_obj
+        # TODO: se target_sheet_id for igual a nossa ID, podemos usar o batchUpdate()/duplicate, que é mais simples e permite
+        #   setar o nome da nova sheet diretamente
+        spreadsheets = self._service.spreadsheets()
+        body = {
+            "destinationSpreadsheetId": target_sheet_id
+        }
+        try:
+            response = spreadsheets.sheets().copyTo(spreadsheetId=self.sheet_id, sheetId=self.get_table_id(), body=body).execute()
+            # copyTo() response is essentially the "sheet properties" dict that is also used in few other requests.
+        except:
+            self._log(f"{self}: failed to duplicate sheet to '{target_sheet_id}': {traceback.format_exc()}", fg="red", ignore_verbose=True)
+            return
+        response = Table(**response)
+        dup_sheet = Sheet(target_sheet_id, response.title, self._credentials, self.verbose)
+        dup_sheet._table_id = response.sheetId
+        return dup_sheet
 
-    @classmethod
-    def preload_service(cls, creds: 'SheetCredentials'):
-        """Preloads the authentication and Google's Resource service using the given credentials.
+    def get_table_id(self):
+        """Gets the table (sheet) ID of this sheet.
 
-        The service is the API used internally to interact with Google Sheets.
-        Preloading this may make later calls to `load()` or `save()` in all sheets faster, since initializing the service takes a while.
+        While ``self.sheet_id`` actually refers to our *spreadsheet* ID, this table ID is our actual *sheet* ID.
+        It is a simple integer ID (instead of the long string alphanumeric spreadsheet ID) that uniquely identifies
+        this sheet within its spreadsheet.
+
+        Usually the sheet's name is used to identify it in the spreadsheet, however the name can be changed (see
+        ``self.rename()``). The sheet (table) ID is set automatically by Google Sheets and can never be changed.
+        Some internal requests to the Sheets API require usage of the sheet-ID instead of its name, thus this can
+        be used.
+
+        The first time this used, a request will be made to the Sheets API in order to query our sheet-ID based on
+        our sheet name. Afterwards, the ID is cached in this instance and will be reused by this method instead of
+        re-doing the request. However, some methods of creating a sheet (such as duplicating it) will also set the
+        sheet's ID, so the request is never needed.
+
+        Returns:
+            int: sheet ID that uniquely identifies this sheet within its spreadsheet. Returns None if we failed to
+            get the ID. Error messages are automatically printed to the terminal.
         """
-        cls._get_service(creds)
+        if self._table_id is None:
+            spreadsheets = self._service.spreadsheets()
+            try:
+                response = spreadsheets.get(spreadsheetId=self.sheet_id).execute()
+            except:
+                self._log(f"{self}: failed to get table-id: {traceback.format_exc()}", fg="red", ignore_verbose=True)
+                return
+            # The spreadsheets.get() JSON response doc has over 3.7k lines. Too much to document here.
+            response = Table(**response)
+            for sheet_data in response.sheets:
+                if sheet_data.properties.title == self.sheet_name:
+                    self._table_id = int(sheet_data.properties.sheetId)
+                    break
+        return self._table_id
+
+    def rename(self, new_sheet_name: str):
+        """Renames this sheet to the given new name.
+
+        If another sheet exists in our spreadsheet with the `new_sheet_name`, this will fail.
+
+        Args:
+            new_sheet_name (str): new name to give to this sheet.
+
+        Returns:
+            bool: indicates if renaming was performed successfully. Error messages are automatically printed
+            to the terminal.
+        """
+        spreadsheets = self._service.spreadsheets()
+        body = {
+            "includeSpreadsheetInResponse": False,
+            "responseIncludeGridData": False,
+            "requests": [
+                {
+                    "updateSheetProperties": {
+                        "fields": "title",
+                        "properties": {
+                            "sheetId": self.get_table_id(),
+                            "title": new_sheet_name
+                        }
+                    }
+                }
+            ],
+        }
+        # This spreadsheets.batchUpdate() doc has over 8k lines for its JSON body, and over 7k
+        # lines for its JSON response. Too much to document here, so we just directly use what we need.
+        # However, it can be used to update pretty much anything from each sheet or from the spreadsheet itself,
+        # including: adding/deleting/duplicate sheets, setting formatting (colors, borders, etc), cell validation
+        # rules, and more.
+        try:
+            response = spreadsheets.batchUpdate(spreadsheetId=self.sheet_id, body=body).execute()
+            response = Table(**response)
+        except Exception:
+            self._log(f"{self}: failed to rename sheet to '{new_sheet_name}': {traceback.format_exc()}", fg="red", ignore_verbose=True)
+            return False
+        self.sheet_name = new_sheet_name
+        return True
 
     @classmethod
     def set_default_credentials(cls, creds: 'SheetCredentials'):
@@ -432,6 +545,9 @@ class SheetCredentials:
     Subclasses of this should implement their actual logic for getting the credentials.
     """
 
+    def __init__(self):
+        self._service: discovery.Resource = None
+
     def get_credentials(self, scopes: list[str]) -> Credentials:
         """Gets the auth credentials for Google API using the given scopes."""
         raise NotImplementedError
@@ -443,7 +559,24 @@ class SheetCredentials:
     def cleanup(self):
         """Cleans up this Credentials object, doing any operation it needs to clean up whatever resources it
         used, such as removing locally stored data."""
-        raise NotImplementedError
+        self._service = None
+
+    def get_service(self) -> discovery.Resource:
+        """Gets the Google's Resource service, used to call Sheets API commands, using these credentials.
+
+        The authenticated service object is cached in this SheetCredentials object, so further calls to this method
+        will return the cached object.
+        """
+        if self._service is None:
+            discovery_url = "https://sheets.googleapis.com/$discovery/rest?version=v4"
+            # For now use the same scopes for all sheets since its easier and there's been no need for custom scopes per-sheet.
+            scopes = [
+                'https://www.googleapis.com/auth/spreadsheets.readonly',
+                'https://www.googleapis.com/auth/spreadsheets'
+            ]
+            google_creds = self.get_credentials(scopes)
+            self._service = discovery.build('sheets', 'v4', credentials=google_creds, discoveryServiceUrl=discovery_url, cache_discovery=False)
+        return self._service
 
 
 class UserLoginCredentials(SheetCredentials):
@@ -484,7 +617,9 @@ class UserLoginCredentials(SheetCredentials):
             else:
                 click.secho("Checking Google user login...", fg="blue")
                 flow = InstalledAppFlow.from_client_secrets_file(self._client_secrets_file_path, scopes)
-                creds = flow.run_local_server(port=0)
+                # NOTE: port was 0 and it worked on python 3.13
+                #   but on python 3.9 it fails... Fixed it by using the method's default port value of 8080
+                creds = flow.run_local_server(port=8080)
             # Save the credentials for the next run
             cache.set_password(self._cache_key, creds.to_json())
             click.secho(f"Successfully authenticated user account '{creds.account}'!", fg="bright_blue")
